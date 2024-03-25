3307be5bfce73dd440ebfa077e80540f41724d93
Phaneendhar Mandala <phaneendharm@gmail.com>
2024-03-24 18:55:40 -0700

    updated versions

--- audit_secrules/security_rules/versions.tf
+++ audit_secrules/security_rules/versions.tf
@@ -1,21 +1,3 @@
-#
-# $Header: pdbcs/no_ship_src/service/terraform/audit/versions.tf
-#
-# versions.tf
-#
-# Copyright (c) 2017, 2023, Oracle and/or its affiliates. 
-#
-#    NAME
-#      versions.tf
-#
-#    DESCRIPTION
-#      Manages minimum required Terraform Version
-#
-#    NOTES
-#
-#    MODIFIED   (MM/DD/YY)
-#    pmandala    01/16/24 - Creation
-#
 
 terraform {
   required_providers {
